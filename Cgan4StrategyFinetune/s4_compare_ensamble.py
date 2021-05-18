c4b49e5d95cde83bbb2e62b8acfa76f8343cfd52
caifengsteven <31440870+caifengsteven@users.noreply.github.com>
2021-05-19 06:12:23 +0800

    xxx

---
+++ Cgan4StrategyFinetune/s4_compare_ensamble.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+g_model = torch.load('./g_model.pkl')
+data = pd.read_csv('./data/hs_300.csv')
+
+# %%
+s = data['CHGPct'].values
+window_size, n = 252, data.shape[0]
+c = int((n - window_size) * .8)
+x_data = np.array([s[i: i + window_size] for i in range(n - window_size)])
+y_data = np.array([s[i + window_size] for i in range(n - window_size)]).reshape((-1, 1))
+
+x_train, x_test = x_data[:c], x_data[c:]
+y_train, y_test = y_data[:c], y_data[c:]
+y_train[y_train > 0] = 1
+y_train[y_train <= 0] = 0
+# %%
+test_fold = np.full((y_train.shape[0]), 0, dtype=int)
+test_fold[:3000] = -1
+
+# %% gbdt case
+from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+import joblib
+
+ps = PredefinedSplit(test_fold)
+
+model = GradientBoostingRegressor(random_state=2021)
+params = {'n_estimators': [20, 50, 100, 200],
+          'learning_rate': [0.001, 0.01, 0.1, 1],
+          'max_depth': [1, 3, 5]}
+gsearch = GridSearchCV(estimator=GradientBoostingClassifier(random_state=2021),
+                       param_grid=params, cv=ps,
+                       scoring='accuracy', n_jobs=-1)
+gsearch.fit(x_train, y_train.ravel())
+
+# %%
+thread = 1/2
+cgan_ensamble_model = joblib.load('./cgan_ensemble_model.pkl')
+base_model = gsearch.best_estimator_
+pred_em = np.array([cgan_model.best_estimator_.predict(x_test) for cgan_model in cgan_ensamble_model]).T
+pred_y = (pred_em.sum(axis = 1) > (pred_em.shape[1] * thread )).astype(int)
+# %%
+plt.figure(figsize=(16, 8))
+plt.plot((y_test.ravel() + 1).cumprod(), label='index')
+plt.plot((pred_y * y_test.ravel() + 1).cumprod(), label='cgan')
+plt.plot((base_model.predict(x_test) * y_test.ravel() + 1).cumprod(), label='base')
+plt.legend()
+plt.show()
